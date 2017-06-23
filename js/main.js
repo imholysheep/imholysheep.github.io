1e9d965d783d45eb9b1a59e69f992c5588ab1b08
imholysheep <niazhao@gmail.com>
2017-06-23 14:50:13 +0800

    Signed-off-by: imholysheep <niazhao@gmail.com>

--- js/main.js
+++ js/main.js
@@ -27,17 +27,17 @@ $(document).ready(function(){
         $(".navigation").removeClass("animated");
     }});
 
-    $('#countdown_dashboard').countDown({
-        targetDate: {
-            'day':      30,
-            'month':    12,
-            'year':     2018,
-            'hour':     23,
-            'min':      59,
-            'sec':      59,
-        },
-        omitWeeks: true
-    });
+    // $('#countdown_dashboard').countDown({
+        // targetDate: {
+            // 'day':      30,
+            // 'month':    12,
+            // 'year':     2018,
+            // 'hour':     23,
+            // 'min':      59,
+            // 'sec':      59,
+        // },
+        // omitWeeks: true
+    // });
 
     $(".about-slider").owlCarousel(
         {
@@ -58,53 +58,53 @@ $(document).ready(function(){
 
 
     //contact form validation
-    $("#contact-form").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            message: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter Your Name",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            message: {
-                required: "Please Write Something",
-                minlength: "Your message must consist of at least 2 characters"
-            },
-            email: "Please enter a valid email address"
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"mail.php",
-                success: function() {
-                    $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
-    });
+    // $("#contact-form").validate({
+        // rules: {
+            // name: {
+                // required: true,
+                // minlength: 2
+            // },
+            // message: {
+                // required: true,
+                // minlength: 2
+            // },
+            // email: {
+                // required: true,
+                // email: true
+            // }
+        // },
+        // messages: {
+            // name: {
+                // required: "Please enter Your Name",
+                // minlength: "Your name must consist of at least 2 characters"
+            // },
+            // message: {
+                // required: "Please Write Something",
+                // minlength: "Your message must consist of at least 2 characters"
+            // },
+            // email: "Please enter a valid email address"
+        // },
+        // submitHandler: function(form) {
+            // $(form).ajaxSubmit({
+                // type:"POST",
+                // data: $(form).serialize(),
+                // url:"mail.php",
+                // success: function() {
+                    // $('#contact-form :input').attr('disabled', 'disabled');
+                    // $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        // $(this).find(':input').attr('disabled', 'disabled');
+                        // $(this).find('label').css('cursor','default');
+                        // $('#success').fadeIn();
+                    // });
+                // },
+                // error: function() {
+                    // $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        // $('#error').fadeIn();
+                    // });
+                // }
+            // });
+        // }
+    // });
 
 });
 
